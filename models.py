1ac39e1cc7a7e6834eae4e44a83f413edebc306d
Suman Mulumudi <smulumudi@gmail.com>
2015-03-28 00:07:22 -0700

    Add confirmed boolean column to SQL database

--- models.py
+++ models.py
@@ -7,11 +7,13 @@ class User(db.Model):
     uuid = db.Column(db.String(), primary_key=True)
     email = db.Column(db.String(), unique=True)
     languages = db.Column(JSON)
+    confirmed = db.Column(db.Boolean())
 
-    def __init__(self, uuid, email, languages):
+    def __init__(self, uuid, email, languages, confirmed=False):
         self.uuid =uuid
         self.email = email
         self.languages = languages
+        self.confirmed = confirmed
 
     def __repr__(self):
         return '<id {}>'.format(self.uuid)
